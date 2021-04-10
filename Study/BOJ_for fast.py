b8aef2252cc2708de3ed6f3cec9167085ddaaf01
Jungmin <ljm9748@naver.com>
2021-04-10 23:12:37 +0900

    0410

--- Study/BOJ_for fast.py
+++ Study/BOJ_for fast.py
@@ -1,7 +1,13 @@
-A,B=map(int, input().split())
-if A==B:
-    print('==')
-elif A>B:
-    print('>')
-else:
-    print('<')
+# A,B=map(int, input().split())
+# if A==B:
+#     print('==')
+# elif A>B:
+#     print('>')
+# else:
+#     print('<')
+
+for tc in range(int(input())):
+    n, string=input().split()
+    for s in string:
+        print(s*int(n), end='')
+    print()
